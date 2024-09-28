44d62c4d85cab2556781b79dee210d0faa45855e
Humberto Oscata <luisoscatanico@gmail.com>
2024-09-28 18:46:19 -0500

    Editing page.tsx to use API for products and users

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const userURL = "http://localhost:8080/user";
+const productURL = "http://localhost:8080/product";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [users, setUsers] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [userStatus, setUserStatus] = useState(false);
+  const [productStatus, setProductStatus] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    if (userStatus) {
+      axios.get(userURL).then((response) => {
+        setUsers(response.data);
+        setUserStatus(false);
+      });
+    }
+  }, [userStatus]);
+
+  useEffect(() => {
+    if (productStatus) {
+      axios.get(productURL).then((response) => {
+        setProducts(response.data);
+        setProductStatus(false);
+      });
+    }
+  }, [productStatus]);
+
+  const onUser = () => setUserStatus(true);
+  const onProduct = () => setProductStatus(true);
+
+  return (
+    <div className="container mx-auto">
+      <header className="p-5">
+        <h2>
+          <strong> PAGE TITLE</strong>
+        </h2>
+        <nav className="py-2 my-2">
+          <ul className="flex flex-row gap-6">
+            <li className="rounded-xl bg-white text-black px-5 py-2">Home</li>
+            <li className="rounded-xl bg-white text-black px-5 py-2">Users</li>
+            <li className="rounded-xl bg-white text-black px-5 py-2">
+              Services
+            </li>
+            <li className="rounded-xl bg-white text-black px-5 py-2">
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="flex w-full">
+        <div className="w-1/2 p-4 bg-blue-100 flex flex-col items-center justify-center">
+          <h2 className="text-xl font-semibold">USERS DATA</h2>
+          <button
+            onClick={onUser}
+            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            GET USERS
+          </button>
+          <div>
+            {users &&
+              users.length > 0 &&
+              users.map((user: any) => (
+                <div
+                  key={user.id}
+                  className="bg-white border-slate-400 rounded-lg	border-2 m-2 p-2"
+                >
+                  <strong>NAME: </strong> {user.name} {user.lastname} <br />
+                  <strong>EMAIL: </strong> {user.email} <br />
+                  <strong>AGE: </strong> {user.age}
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="w-1/2 p-4 bg-green-100 flex flex-col items-center justify-center">
+          <h2 className="text-xl font-semibold">PRODUCTS DATA</h2>
+          <button
+            onClick={onProduct}
+            className="mt-2 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
           >
-            Read our docs
-          </a>
+            GET PRODUCTS
+          </button>
+          <div>
+            {products &&
+              products.length > 0 &&
+              products.map((product: any) => (
+                <div
+                  key={product.id}
+                  className="bg-white border-slate-400 rounded-lg	border-2 m-2 p-2"
+                >
+                  <strong>PRODUCT: </strong> {product.product} <br />
+                  <strong>CATEGORY: </strong> {product.category} <br />
+                  <strong>PRICE: </strong> {product.price} <br />
+                  <strong>STOCK: </strong> {product.stock}
+                </div>
+              ))}
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
